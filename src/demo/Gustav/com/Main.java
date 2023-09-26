be7975876756529f073efb56dd986f7af43196a1
GustavParment <gustav.parment1993@gmail.com>
2023-09-26 11:25:24 +0200

    nestled foor/loop

--- src/demo/Gustav/com/Main.java
+++ src/demo/Gustav/com/Main.java
@@ -4,11 +4,29 @@ import java.util.Random;
 import java.util.Scanner;
 
 public class Main {
-    public static void main(String[] args) {
+    public static void main(String[] args){
 
+        Scanner sc = new Scanner(System.in);
 
+        int rounds =5;
 
+        System.out.println("How many dice?");
+        int dice = sc.nextInt();
+        System.out.println("You picked: " + dice);
 
+        for (int i = 0; i < rounds; i++) {
+            System.out.println("Round: " + i);
+
+            // Iterate Dice
+            for (int j = 0; j < dice ; j++) {
+                Random random = new Random();
+
+                int score = random.nextInt(6) + 1;
+                System.out.println("Throwing die " + score);
+
+            }
+
+        }
 
 
     }
